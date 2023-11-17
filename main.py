d318f5b56c995c2e03d67c9a4f42963db9254414
Taborda-2010 <125746049+Taborda-2010@users.noreply.github.com>
2023-11-17 11:47:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #import networkx as nx
 
@@ -32,10 +33,10 @@ plt.title('Average Overall Rating by Nationality')
 st.pyplot(plt)
 
 # Display Heatmap of Potential vs Overall Rating
-#st.write('Heatmap of Potential vs Overall Rating:')
-#plt.figure(figsize=(10, 5))
-#sns.heatmap(df[['Potential', 'Overall']].corr(), annot=True, cmap='coolwarm')
-#st.pyplot(plt)
+st.write('Heatmap of Potential vs Overall Rating:')
+plt.figure(figsize=(10, 5))
+sns.heatmap(df[['Potential', 'Overall']].corr(), annot=True, cmap='coolwarm')
+st.pyplot(plt)
 
 # Display Player Distribution by Club
 st.write('Player Distribution by Club:')
@@ -44,14 +45,3 @@ df['Club'].value_counts().plot(kind='pie', autopct='%1.1f%%')
 plt.title('Player Distribution by Club')
 st.pyplot(plt)
 
-# Display Graph of Connections between Players based on Similarity in Overall Rating
-#st.write('Graph of Connections between Players based on Similarity in Overall Rating:')
-#G = nx.Graph()
-#edges = df[['Name', 'Overall']].values.tolist()
-#for edge in edges:
-#    if edge[0] != edge[1]:
-#        G.add_edge(edge[0], edge[1], weight=abs(edge[2] - edge[3]))
-
-pos = graphviz_layout(G, prog='neato')
-nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=10)
-st.pyplot(plt)
